a02458091342962472710269aee1074cccd9bd4d
root <root@s21.myguest.virtualbox.org>
2023-12-05 15:18:09 +0300

    test controller do not work. 3DViewer test work

--- src/tests/3DViewer_test.cc
+++ src/tests/3DViewer_test.cc
@@ -6,168 +6,179 @@
 #include <cstring>
 #include <string>
 
-#include "../model/s21_aff_transform.h"
-#include "../model/s21_parser_obj.h"
+#include "../controller/s21_controller_obj.h"
 #include <gtest/gtest.h>
 
 namespace s21 {
 
 TEST(MainTest, Cube) {
-  s21::ObjT obj;
-  s21::ParserObj parser;
-  s21::AffTransform transform;
-  parser.StartPars("obj/Low-Poly-Racing-Car.obj", &obj);
-  s21::Controller::getInstance()->setData(&obj, &parser, &transform);
-  EXPECT_EQ(obj.getCountFacets(), 18);
-  EXPECT_EQ(obj.getCountVertexes(), 8);
-}
-
-TEST(MainTest, EmptyCube) {
-  s21::ObjT obj;
-  s21::ParserObj parser;
-  s21::AffTransform transform;
-  parser.StartPars("obj/Low-Poly-Racing-Car.obj", &obj);
-  s21::Controller::getInstance()->setData(&obj, &parser, &transform);
-  EXPECT_EQ(obj.getCountFacets(), 0);
-  EXPECT_EQ(obj.getCountVertexes(), 0);
+    s21::ObjT obj;
+    s21::ParserObj parser;
+    const char *file = "/home/s21/3DViewer2_0/src/tests/obj/Low-Poly-Racing-Car.obj";
+    parser.StartPars(file, &obj);
+    EXPECT_EQ(obj.count_of_vertexes, 1485);
+    EXPECT_EQ(obj.count_of_facets, 2718);
 }
 
 TEST(AffTransformTest, MoveX) {
   s21::ObjT obj;
   s21::AffTransform transform;
+  obj.count_of_vertexes = 6;
+  obj.count_of_facets = 0;
+  obj.vertexes = new double[obj.count_of_vertexes];
+  obj.vertexes[0] = 1.0;
+  obj.vertexes[1] = 2.0;
+  obj.vertexes[2] = 3.0;
+  obj.vertexes[3] = -1.0;
+  obj.vertexes[4] = -2.0;
+  obj.vertexes[5] = -3.0;
   transform.MoveX(&obj, 2.0);
-  obj.setCountVertexes(6);
-  obj.setCountFacets(0);
-  obj.allocateVertexes();
-  obj.pushVertex(1.0, 0);
-  obj.pushVertex(2.0, 1);
-  obj.pushVertex(3.0, 2);
-  obj.pushVertex(-1.0, 3);
-  obj.pushVertex(-2.0, 4);
-  obj.pushVertex(-3.0, 5);
-
-  const auto* vertexes = obj.getVertexes();
+  const double* vertexes = obj.vertexes;
   EXPECT_DOUBLE_EQ(vertexes[0], 3.0);
   EXPECT_DOUBLE_EQ(vertexes[1], 2.0);
   EXPECT_DOUBLE_EQ(vertexes[2], 3.0);
   EXPECT_DOUBLE_EQ(vertexes[3], 1.0);
   EXPECT_DOUBLE_EQ(vertexes[4], -2.0);
   EXPECT_DOUBLE_EQ(vertexes[5], -3.0);
+  delete[] obj.vertexes;
+}
+
+TEST(AffTransformTest, MoveY) {
+  s21::ObjT obj;
+  s21::AffTransform transform;
+  obj.count_of_vertexes = 6;
+  obj.count_of_facets = 0;
+  obj.vertexes = new double[obj.count_of_vertexes];
+  obj.vertexes[0] = 1.0;
+  obj.vertexes[1] = 2.0;
+  obj.vertexes[2] = 3.0;
+  obj.vertexes[3] = -1.0;
+  obj.vertexes[4] = -2.0;
+  obj.vertexes[5] = -3.0;
+  transform.MoveY(&obj, 3.0);
+  const double* vertexes = obj.vertexes;
+  EXPECT_DOUBLE_EQ(vertexes[0], 1.0);
+  EXPECT_DOUBLE_EQ(vertexes[1], 5.0);
+  EXPECT_DOUBLE_EQ(vertexes[2], 3.0);
+  EXPECT_DOUBLE_EQ(vertexes[3], -1.0);
+  EXPECT_DOUBLE_EQ(vertexes[4], 1.0);
+  EXPECT_DOUBLE_EQ(vertexes[5], -3.0);
+  delete[] obj.vertexes;
 }
 
 TEST(AffTransformTest, MoveZ) {
   s21::ObjT obj;
   s21::AffTransform transform;
-  transform.MoveZ(&obj, 2.0);
-  obj.setCountVertexes(6);
-  obj.setCountFacets(0);
-  obj.allocateVertexes();
-  obj.pushVertex(1.0, 0);
-  obj.pushVertex(2.0, 1);
-  obj.pushVertex(3.0, 2);
-  obj.pushVertex(-1.0, 3);
-  obj.pushVertex(-2.0, 4);
-  obj.pushVertex(-3.0, 5);
-
-  const auto* vertexes = obj.getVertexes();
-  EXPECT_DOUBLE_EQ(vertexes[0], 3.0);
+  obj.count_of_vertexes = 6;
+  obj.count_of_facets = 0;
+  obj.vertexes = new double[obj.count_of_vertexes];
+  obj.vertexes[0] = 1.0;
+  obj.vertexes[1] = 2.0;
+  obj.vertexes[2] = 3.0;
+  obj.vertexes[3] = -1.0;
+  obj.vertexes[4] = -2.0;
+  obj.vertexes[5] = -3.0;
+  transform.MoveZ(&obj, 4.0);
+  const double* vertexes = obj.vertexes;
+  EXPECT_DOUBLE_EQ(vertexes[0], 1.0);
   EXPECT_DOUBLE_EQ(vertexes[1], 2.0);
-  EXPECT_DOUBLE_EQ(vertexes[2], 3.0);
-  EXPECT_DOUBLE_EQ(vertexes[3], 1.0);
+  EXPECT_DOUBLE_EQ(vertexes[2], 7.0);
+  EXPECT_DOUBLE_EQ(vertexes[3], -1.0);
   EXPECT_DOUBLE_EQ(vertexes[4], -2.0);
-  EXPECT_DOUBLE_EQ(vertexes[5], -3.0);
+  EXPECT_DOUBLE_EQ(vertexes[5], 1.0);
+  delete[] obj.vertexes;
 }
 
 TEST(AffTransformTest, TurnX) {
   s21::ObjT obj;
   s21::AffTransform transform;
-  transform.TurnX(&obj, 2.0);
-  obj.setCountVertexes(6);
-  obj.setCountFacets(0);
-  obj.allocateVertexes();
-  obj.pushVertex(1.0, 0);
-  obj.pushVertex(0.0, 1);
-  obj.pushVertex(0.0, 2);
-  obj.pushVertex(0.0, 3);
-  obj.pushVertex(1.0, 4);
-  obj.pushVertex(0.0, 5);
-
-  const auto* vertexes = obj.getVertexes();
+  obj.count_of_vertexes = 6;
+  obj.count_of_facets = 0;
+  obj.vertexes = new double[obj.count_of_vertexes];
+  obj.vertexes[0] = 1.0;
+  obj.vertexes[1] = 0.0;
+  obj.vertexes[2] = 0.0;
+  obj.vertexes[3] = 0.0;
+  obj.vertexes[4] = 1.0;
+  obj.vertexes[5] = 0.0;
+  transform.TurnX(&obj, M_PI / 2.0);
+  const double* vertexes = obj.vertexes;
   EXPECT_NEAR(vertexes[0], 1.0, 1e-6);
   EXPECT_NEAR(vertexes[1], 0.0, 1e-6);
   EXPECT_NEAR(vertexes[2], 0.0, 1e-6);
   EXPECT_NEAR(vertexes[3], 0.0, 1e-6);
-  EXPECT_NEAR(vertexes[4], 0.0, 1e-6);
-  EXPECT_NEAR(vertexes[5], -1.0, 1e-6);
+  EXPECT_NEAR(vertexes[4], 0.99962421685, 1e-6);
+  EXPECT_NEAR(vertexes[5], -0.0274121335920, 1e-6);
+  delete[] obj.vertexes;
 }
 
 TEST(AffTransformTest, TurnY) {
   s21::ObjT obj;
   s21::AffTransform transform;
-  transform.TurnY(&obj, 2.0);
-  obj.setCountVertexes(6);
-  obj.setCountFacets(0);
-  obj.allocateVertexes();
-  obj.pushVertex(1.0, 0);
-  obj.pushVertex(0.0, 1);
-  obj.pushVertex(0.0, 2);
-  obj.pushVertex(0.0, 3);
-  obj.pushVertex(0.0, 4);
-  obj.pushVertex(1.0, 5);
-
-  const auto* vertexes = obj.getVertexes();
-  EXPECT_NEAR(vertexes[0], 0.0, 1e-6);
+  obj.count_of_vertexes = 6;
+  obj.count_of_facets = 0;
+  obj.vertexes = new double[obj.count_of_vertexes];
+  obj.vertexes[0] = 1.0;
+  obj.vertexes[1] = 0.0;
+  obj.vertexes[2] = 0.0;
+  obj.vertexes[3] = 0.0;
+  obj.vertexes[4] = 0.0;
+  obj.vertexes[5] = 1.0;
+  transform.TurnY(&obj, M_PI / 2.0);
+  const double* vertexes = obj.vertexes;
+  EXPECT_NEAR(vertexes[0], 0.999624216859, 1e-6);
   EXPECT_NEAR(vertexes[1], 0.0, 1e-6);
-  EXPECT_NEAR(vertexes[2], -1.0, 1e-6);
-  EXPECT_NEAR(vertexes[3], 1.0, 1e-6);
+  EXPECT_NEAR(vertexes[2], 0.0274121335920, 1e-6);
+  EXPECT_NEAR(vertexes[3], -0.027412133592, 1e-6);
   EXPECT_NEAR(vertexes[4], 0.0, 1e-6);
-  EXPECT_NEAR(vertexes[5], 0.0, 1e-6);
+  EXPECT_NEAR(vertexes[5], 0.9996242168, 1e-6);
+  delete[] obj.vertexes;
 }
 
 TEST(AffTransformTest, TurnZ) {
   s21::ObjT obj;
   s21::AffTransform transform;
-  transform.TurnZ(&obj, 2.0);
-  obj.setCountVertexes(6);
-  obj.setCountFacets(0);
-  obj.allocateVertexes();
-  obj.pushVertex(1.0, 0);
-  obj.pushVertex(0.0, 1);
-  obj.pushVertex(0.0, 2);
-  obj.pushVertex(0.0, 3);
-  obj.pushVertex(1.0, 4);
-  obj.pushVertex(0.0, 5);
-
-  const auto* vertexes = obj.getVertexes();
-  EXPECT_NEAR(vertexes[0], 0.0, 1e-6);
-  EXPECT_NEAR(vertexes[1], -1.0, 1e-6);
+  obj.count_of_vertexes = 6;
+  obj.count_of_facets = 0;
+  obj.vertexes = new double[obj.count_of_vertexes];
+  obj.vertexes[0] = 1.0;
+  obj.vertexes[1] = 0.0;
+  obj.vertexes[2] = 0.0;
+  obj.vertexes[3] = 0.0;
+  obj.vertexes[4] = 0.0;
+  obj.vertexes[5] = 1.0;
+  transform.TurnZ(&obj, M_PI / 2.0);
+  const double* vertexes = obj.vertexes;
+  EXPECT_NEAR(vertexes[0], 0.999624216, 1e-6);
+  EXPECT_NEAR(vertexes[1], -0.02741213, 1e-6);
   EXPECT_NEAR(vertexes[2], 0.0, 1e-6);
-  EXPECT_NEAR(vertexes[3], 1.0, 1e-6);
+  EXPECT_NEAR(vertexes[3], 0.0, 1e-6);
   EXPECT_NEAR(vertexes[4], 0.0, 1e-6);
-  EXPECT_NEAR(vertexes[5], 0.0, 1e-6);
+  EXPECT_NEAR(vertexes[5], 1.0, 1e-6);
+  delete[] obj.vertexes;
 }
 
 TEST(AffTransformTest, ChangeSize) {
   s21::ObjT obj;
   s21::AffTransform transform;
+  obj.count_of_vertexes = 6;
+  obj.count_of_facets = 0;
+  obj.vertexes = new double[obj.count_of_vertexes];
+  obj.vertexes[0] = 1.0;
+  obj.vertexes[1] = 2.0;
+  obj.vertexes[2] = 3.0;
+  obj.vertexes[3] = -1.0;
+  obj.vertexes[4] = -2.0;
+  obj.vertexes[5] = -3.0;
   transform.Scale(&obj, 2.0);
-  obj.setCountVertexes(6);
-  obj.setCountFacets(0);
-  obj.allocateVertexes();
-  obj.pushVertex(1.0, 0);
-  obj.pushVertex(2.0, 1);
-  obj.pushVertex(3.0, 2);
-  obj.pushVertex(-1.0, 3);
-  obj.pushVertex(-2.0, 4);
-  obj.pushVertex(-3.0, 5);
-
-  const auto* vertexes = obj.getVertexes();
+  const double* vertexes = obj.vertexes;
   EXPECT_DOUBLE_EQ(vertexes[0], 2.0);
   EXPECT_DOUBLE_EQ(vertexes[1], 4.0);
   EXPECT_DOUBLE_EQ(vertexes[2], 6.0);
   EXPECT_DOUBLE_EQ(vertexes[3], -2.0);
   EXPECT_DOUBLE_EQ(vertexes[4], -4.0);
   EXPECT_DOUBLE_EQ(vertexes[5], -6.0);
+  delete[] obj.vertexes;
 }
 
 }
--- src/tests/s21_controller_tests.cc
+++ src/tests/s21_controller_tests.cc
@@ -7,7 +7,21 @@ TEST(ControllerTest, GetDataObj) {
     s21::Controller * controller_obj_ = nullptr;
     controller_obj_ = s21::Controller::getInstance();
     s21::Facade facade;
+    const char* file = "/home/s21/3DViewer2_0/src/tests/obj/Low-Poly-Racing-Car.obj";
     controller_obj_->SetModel(&facade);
-    controller_obj_->OpenObj("obj/Low-Poly-Racing-Car.obj");
+    controller_obj_->OpenObj(file);
+    controller_obj_ = nullptr;
+    EXPECT_EQ(controller_obj_, nullptr);   
+    // EXPECT_EQ(facade.count_of_vertexes, 1485);
+    // EXPECT_EQ(facade.count_of_facets, 2718); 
+}
+
+TEST(ControllerTest, GetData) {
+    s21::Controller * controller_obj_ = nullptr;
+    controller_obj_ = s21::Controller::getInstance();
+    s21::Facade facade;
+    // const char* file = "obj/Low-Poly-Racing-Car.obj";
+    controller_obj_->SetModel(&facade);
+    controller_obj_->MoveObj('x', 0.2);
     EXPECT_EQ(controller_obj_, nullptr);    
 }
