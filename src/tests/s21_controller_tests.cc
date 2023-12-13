8b73d027f0ff3b3e2874a21637f1306877c17d9a
root <root@DESKTOP-GRCI8K8.localdomain>
2023-12-13 13:58:43 +0300

    gcov_tests add

--- src/tests/s21_controller_tests.cc
+++ src/tests/s21_controller_tests.cc
@@ -69,3 +69,20 @@ TEST(ControllerTest, ChangeSize) {
   EXPECT_DOUBLE_EQ(controller_obj_->GetObject().vertexes[1], 10.594722032546999);
   EXPECT_DOUBLE_EQ(controller_obj_->GetObject().vertexes[2], -25.211155414581299);
 }
+
+TEST(ControllerTest, ResetObj) {
+  s21::Controller* controller_obj_;
+  controller_obj_ = s21::Controller::getInstance();
+  s21::Facade facade;
+  const char* file = "./tests/obj/Low-Poly-Racing-Car.obj";
+  controller_obj_->SetModel(&facade);
+  controller_obj_->OpenObj(file);
+  EXPECT_DOUBLE_EQ(controller_obj_->GetObject().vertexes[1], 2.1189444065093999);
+  EXPECT_DOUBLE_EQ(controller_obj_->GetObject().vertexes[2], -5.0422310829162598);
+  controller_obj_->ScaleObj('0', 5.0);
+  EXPECT_DOUBLE_EQ(controller_obj_->GetObject().vertexes[1], 10.594722032546999);
+  EXPECT_DOUBLE_EQ(controller_obj_->GetObject().vertexes[2], -25.211155414581299);
+  controller_obj_->ResetObj();
+  EXPECT_DOUBLE_EQ(controller_obj_->GetObject().vertexes[1], 2.1189444065093999);
+  EXPECT_DOUBLE_EQ(controller_obj_->GetObject().vertexes[2], -5.0422310829162598);
+}
